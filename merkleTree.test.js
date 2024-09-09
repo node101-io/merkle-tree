a4023f46a812d7610a50bd4d6821adeccecefd53
tunntun <mervehtnozcan@gmail.com>
2024-09-09 20:44:45 +0200

    test create merkle tree

--- merkleTree.test.js
+++ merkleTree.test.js
@@ -1,28 +1,88 @@
 const merkleTree = require('./merkleTree');
 
-test('properly creates a merkle tree', (done) => {
-  const textArray = ['test1', 'test2', 'test3', 'test4', 'test5'];
+describe.each([
+  {
+    leavesArray: ['test1'],
+    expectedMerkleTree: {
+      tree: ['1b4f0e9851971998e732078544c96b36c3d01cedf7caa332359d6f1d83567014'],
+      root: '1b4f0e9851971998e732078544c96b36c3d01cedf7caa332359d6f1d83567014',
+      leavesArray: ['test1']
+    }
+  },
+  {
+    leavesArray: ['test1', 'test2'],
+    expectedMerkleTree: {
+      tree: [
+        '1b4f0e9851971998e732078544c96b36c3d01cedf7caa332359d6f1d83567014',
+        '60303ae22b998861bce3b28f33eec1be758a213c86c93c076dbe9f558c11c752',
+        '2f297f1520dfd4d6a9b680536568fd3aad16a8c2d7067b654ea06dd931bccd51'
+      ],
+      root: '2f297f1520dfd4d6a9b680536568fd3aad16a8c2d7067b654ea06dd931bccd51',
+      leavesArray: ['test1', 'test2']
+    }
+  },
+  {
+    leavesArray: ['test1', 'test2', 'test3'],
+    expectedMerkleTree: {
+      tree: [
+        '1b4f0e9851971998e732078544c96b36c3d01cedf7caa332359d6f1d83567014',
+        '60303ae22b998861bce3b28f33eec1be758a213c86c93c076dbe9f558c11c752',
+        'fd61a03af4f77d870fc21e05e7e80678095c92d808cfb3b5c279ee04c74aca13',
+        'fd61a03af4f77d870fc21e05e7e80678095c92d808cfb3b5c279ee04c74aca13',
+        '2f297f1520dfd4d6a9b680536568fd3aad16a8c2d7067b654ea06dd931bccd51',
+        '000b7a5fc83fa7fb1e405b836daf3488d00ac42cb7fc5a917840e91ddc651661',
+        'd6aaf9c71e15f57bbd132955709520fe633c6547b2bc0ceb66baaaff71f9ab61'
 
-  const expectedMerkleTree = {
-    tree: [
-      'f9c2bf2b8e8275541f35c8d03cbcf57d8b73eb24bbaac832e113a20ba9f3b868',
-      '73475cb40a568e8da8a5a25ef9f36a8f638316d8e3ac5f6c1c7fa975e8f1d863',
-      '4e07408562bedb8b60ce05c1decfe3ad16b72230960c90b78b7420527d07053f',
-      'ef797c8118f02d4c2b1dd2f098c51b56cc7732778f93d5474120e04d6c7b202e',
-      '2ef7bde608ce5404e97d5f042f95f89f1c2328711cfdd8a7d51f2fdb8a10d989',
-      '5feceb66ffc86f38d952786c6d696c79c7a4b8d9653a6a2a823965a0057d2cb0',
-      '6b86b273ff34fce19d6b804eff5a3f574d25711d3d48aa1d2f1a1f2d668c8a9d',
-      'd4735e3a265e16eee03f59718b9b5d03b5e2a6ce9697a0e42f5cf7a29977c5d1',
-      '2d711642b726b04401627ca9fbac32f5c2855c1b1fd47d60f29c12f69fa7e0db'
-    ],
-    root: '2d711642b726b04401627ca9fbac32f5c2855c1b1fd47d60f29c12f69fa7e0db',
-    leavesArray: ['test1', 'test2', 'test3', 'test4', 'test5']
-  };
+      ],
+      root: 'd6aaf9c71e15f57bbd132955709520fe633c6547b2bc0ceb66baaaff71f9ab61',
+      leavesArray: ['test1', 'test2', 'test3']
+    }
+  },
+  {
+    leavesArray: ['test1', 'test2', 'test3', 'test4'],
+    expectedMerkleTree: {
+      tree: [
+        '1b4f0e9851971998e732078544c96b36c3d01cedf7caa332359d6f1d83567014',
+        '60303ae22b998861bce3b28f33eec1be758a213c86c93c076dbe9f558c11c752',
+        'fd61a03af4f77d870fc21e05e7e80678095c92d808cfb3b5c279ee04c74aca13',
+        'a4e624d686e03ed2767c0abd85c14426b0b1157d2ce81d27bb4fe4f6f01d688a',
+        '2f297f1520dfd4d6a9b680536568fd3aad16a8c2d7067b654ea06dd931bccd51',
+        '400c4387b6200c3cdc99a4a6efa396cceb74640d3ea93c0e96a84c0a396de8c9',
+        'b437a3f04a2410505819386b059d09fab957c0ae2046caeab93beaa91e33352f'
 
-  merkleTree.createMerkleTree(textArray, (err, merkleTree) => {
-    if(err)
-      return done(err);
+      ],
+      root: 'b437a3f04a2410505819386b059d09fab957c0ae2046caeab93beaa91e33352f',
+      leavesArray: ['test1', 'test2', 'test3', 'test4']
+    }
+  },
+  {
+    leavesArray: ['test1', 'test2', 'test3', 'test4', 'test5'],
+    expectedMerkleTree: {
+      tree: [
+        '1b4f0e9851971998e732078544c96b36c3d01cedf7caa332359d6f1d83567014', //
+        '60303ae22b998861bce3b28f33eec1be758a213c86c93c076dbe9f558c11c752', //
+        'fd61a03af4f77d870fc21e05e7e80678095c92d808cfb3b5c279ee04c74aca13', //
+        'a4e624d686e03ed2767c0abd85c14426b0b1157d2ce81d27bb4fe4f6f01d688a', //
+        'a140c0c1eda2def2b830363ba362aa4d7d255c262960544821f556e16661b6ff', //
+        'a140c0c1eda2def2b830363ba362aa4d7d255c262960544821f556e16661b6ff', //
+        '2f297f1520dfd4d6a9b680536568fd3aad16a8c2d7067b654ea06dd931bccd51',
+        '400c4387b6200c3cdc99a4a6efa396cceb74640d3ea93c0e96a84c0a396de8c9',
+        '5becda531601ec4b4aeb5cdd05b0c891946fa6fbc75c974a643723c6d7e60b24',
+        '5becda531601ec4b4aeb5cdd05b0c891946fa6fbc75c974a643723c6d7e60b24',
+        'b437a3f04a2410505819386b059d09fab957c0ae2046caeab93beaa91e33352f',
+        '9c9ead8b820cb402c29345f1282a6ab00fa23b6f3f9b23cff537d7c6acb2c8ac',
+        '6884a11aa2f9382de974dbb9e7277ee38a6636646bba1317739f3d8e0daf5fab'
 
-    expect(merkleTree).toEqual(expectedMerkleTree);
-  });
+      ],
+      root: '6884a11aa2f9382de974dbb9e7277ee38a6636646bba1317739f3d8e0daf5fab',
+      leavesArray: ['test1', 'test2', 'test3', 'test4', 'test5']
+    }
+  }
+])('create merkle tree for $leavesArray', ({leavesArray, expectedMerkleTree}) => {
+    test(`returns the correct Merkle tree`, () => {
+      merkleTree.createMerkleTree(leavesArray, (err, merkleTree) => {
+        expect(err).toEqual(null);
+        expect(merkleTree).toEqual(expectedMerkleTree);
+      });
+    });
 });
